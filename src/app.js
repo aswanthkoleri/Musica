c101c626b544883bbf170f1343b1e6f2600e3e81
aswanthkoleri <iit2016105@iiita.ac.in>
2018-07-04 22:22:11 +0530

    Add basic files

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './assets/styles.scss';
+
+/* const Layout =(props)=>{
+    return (
+        <div>
+        {props.children}
+        </div>
+    ); 
+};
+
+
+ReactDOM.render((<Layout>
+    <IndecisionApp/>
+</Layout>),document.getElementById('app')); */
+
+ReactDOM.render((<p>This a React JS boiler plate for projects</p>),document.getElementById('app'));
