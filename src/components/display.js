ff9283a5b63474dbadf0eb09fe1ba655ce8fe0ce
aswanthkoleri <iit2016105@iiita.ac.in>
2018-07-06 06:17:14 +0530

    Add some UI Fixes

--- src/components/display.js
+++ src/components/display.js
@@ -3,13 +3,13 @@ import React from 'react';
 export default class Display extends React.Component {
     render() {
         const content= (
-            <div>
+            <div className="result">
             {
                 this.props.results.map((results,index)=>{
             return ( 
-            <div>
-             <h3 key={index}>{results.snippet.title}</h3>
-             <iframe src={`https://www.download-mp3-youtube.com/api/?api_key=MjI5MTIxMzQy&format=mp3&video_id=${results.id.videoId}&button_color=dddddd&text_color=444444`}></iframe>
+            <div key={index}>
+             <h3 >{results.snippet.title}</h3>
+             <iframe className="downloadButton" src={`https://www.download-mp3-youtube.com/api/?api_key=MjI5MTIxMzQy&format=mp3&video_id=${results.id.videoId}&button_color=2b6fa7&text_color=#e7e7e7`}></iframe>
              </div>
            );
                 })
