27b53f5e642f23741207b479eadfc4f7d24150c1
aswanthkoleri <iit2016105@iiita.ac.in>
2018-07-06 06:54:50 +0530

    Prepared for production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 
 const path = require('path');
-module.exports = {
+module.exports=(env)=> {
+    console.log('env',env);
+    const isProduction = env === 'production';
+    return {
     entry : './src/app.js',
     output : {
         path : path.join(__dirname,'public'),
@@ -30,8 +33,10 @@ module.exports = {
         }
     ]
     },
-    devtool : 'cheap-module-eval-source-map',
+    devtool : isProduction? 'source-map' : 'cheap-module-eval-source-map',
     devServer : {
         contentBase : path.join(__dirname,'public')
     }
 };
+
+}
