154253e96f74d7fedeccc34e418af06a381f63a5
aswanthkoleri <iit2016105@iiita.ac.in>
2018-07-06 07:14:30 +0530

    Tweak webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports=(env)=> {
     console.log('env',env);
     const isProduction = env === 'production';
+    const cssExtract =new ExtractTextPlugin('styles.css');
     return {
     entry : './src/app.js',
     output : {
@@ -25,15 +28,29 @@ module.exports=(env)=> {
         },
         {
             test : /\.s?css$/,
-            use : [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
+            use : cssExtract.extract({
+                use : [
+                    {
+                        loader: 'css-loader',
+                        options: {
+                          sourceMap: true
+                        }
+                      },
+                      {
+                        loader: 'sass-loader',
+                        options: {
+                          sourceMap: true
+                        }
+                      }
+                ]
+            })
         }
     ]
     },
-    devtool : isProduction? 'source-map' : 'cheap-module-eval-source-map',
+    plugins : [
+        cssExtract
+    ],
+    devtool : isProduction? 'source-map' : 'inline-source-map',
     devServer : {
         contentBase : path.join(__dirname,'public')
     }
